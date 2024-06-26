8690fad535a2bff87528ea4b5fbdfff2e57b5610
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-06-26 14:34:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+let son = prompt(
+    "Menu: \n 1) Coca-cola - 8000uzs \n 2) Snickers - 7000uzs \n 3) Fuse-tea - 6000uzs \n 4) Ruchka - 5000uzs \n 5) Kitob - 20000uzs \n 6) Qalam - 4000uzs \n 7) Pepsi - 9000uzs \n 8) Kitkat - 12000uzs \n 9) Maxfun - 18000uzs \n 10) Airpods - 30000uzs \n"
+);
+
+if (son >= 1 && 10 >= son){
+    alert("Siz tanladz" + son + " chi nomer");
+    let pul = +prompt("Pul tiqing");
+
+    if (son === "1" && pul >= 8000) {
+        console.log("=============================");
+        console.log("narsa: Coca-cola");
+        console.log("narx: 8000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "2" && pul >= 7000) {
+    console.log("=============================");
+    console.log("narsa: Snickers");
+    console.log("narx: 7000 uzs");
+    console.log("haridingiz uchun rahmat!");
+    console.log("=============================");
+    } else if (son === "3" && pul >= 6000) {
+        console.log("=============================");
+        console.log("narsa: Fuse-tea");
+        console.log("narx: 6000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "4" && pul >= 5000) {
+        console.log("=============================");
+        console.log("narsa: Ruchka");
+        console.log("narx: 5000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "5" && pul >= 20000) {
+        console.log("=============================");
+        console.log("narsa: Kitob");
+        console.log("narx: 20000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "6" && pul >= 4000) {
+        console.log("=============================");
+        console.log("narsa: Qalam");
+        console.log("narx: 4000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "7" && pul >= 9000) {
+        console.log("=============================");
+        console.log("narsa: Pepsi");
+        console.log("narx: 9000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "8" && pul >= 12000) {
+        console.log("=============================");
+        console.log("narsa: Kitkat");
+        console.log("narx: 12000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "9" && pul >= 18000) {
+        console.log("=============================");
+        console.log("narsa: Maxfun");
+        console.log("narx: 18000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    } else if (son === "10" && pul >= 7000) {
+        console.log("=============================");
+        console.log("narsa: Airpods");
+        console.log("narx: 30000 uzs");
+        console.log("haridingiz uchun rahmat!");
+        console.log("=============================");
+    }
+    
+}
